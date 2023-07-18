1f873da0ab0be9674ad36ab129573a9b4b81bb6d
JEONG HYUN SEOK <hyunseok.jeong@annotation-ai.com>
2023-07-18 10:58:34 +0900

    feat: streaming on 02

--- 02/02.py
+++ 02/02.py
@@ -1,24 +1,30 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationSummaryBufferMemory
-from prompt_toolkit import prompt
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
+from prompt_toolkit import prompt
 from rich.console import Console
 console = Console()
 
-llm = ChatOpenAI()
+llm = ChatOpenAI(
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
+    temperature=0,
+)
+
 conversation = ConversationChain(
     llm=llm,
-    memory=ConversationSummaryBufferMemory(
-        llm=ChatOpenAI(), max_token_limit=2048
-    ),
+    memory=ConversationSummaryBufferMemory(llm=ChatOpenAI(), max_token_limit=2048),
     verbose=False,
 )
 
 while True:
-  user_input = prompt(console.print('Human: ', style="green"))
-  print()
-  
-  result = conversation.predict(input=user_input)
-  print(console.print('AI: ', style="green"), result)
-  print()
+    print()
+    user_input = prompt(console.print("Human: ", style="green"))
+    print()
+
+    console.print("AI: ", style="green")
+    conversation.run(input=user_input)
+    print()
+
