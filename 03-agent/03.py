398cfe0ad9d30cd92ab0f9a005cb24bb82ee51f2
JEONG HYUN SEOK <hyunseok.jeong@annotation-ai.com>
2023-07-18 13:14:14 +0900

    feat: final answer only

--- 03-agent/03.py
+++ 03-agent/03.py
@@ -1,8 +1,14 @@
 # LLM related
 from langchain.chat_models import ChatOpenAI
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.streaming_stdout import (
+    StreamingStdOutCallbackHandler,
+)  # output all
+from langchain.callbacks.streaming_stdout_final_only import (
+    FinalStreamingStdOutCallbackHandler,
+)  # output final only
 from langchain.memory import ConversationBufferMemory
 from langchain.agents import Tool, AgentType, initialize_agent
+
 from langchain.tools import DuckDuckGoSearchRun
 
 # utilities
@@ -14,12 +20,13 @@ console = Console()
 # Code
 # 1. 메모리, LLM, 툴 세가지를 정의한다.
 memory = ConversationBufferMemory(
-    memory_key="chat_history", # 이 값이 필수이다. 왜 그럴까?
+    memory_key="chat_history",  # 이 값이 필수이다. 왜 그럴까?
     return_messages=True,
 )
 llm = ChatOpenAI(
     streaming=True,
-    callbacks=[StreamingStdOutCallbackHandler()],
+    # callbacks=[StreamingStdOutCallbackHandler()], # output all
+    callbacks=[FinalStreamingStdOutCallbackHandler()],  # output final only
     temperature=0,
 )
 search = DuckDuckGoSearchRun()
@@ -35,11 +42,12 @@ agent_chain = initialize_agent(
     tools,
     llm,
     agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
-    verbose=True,
+    # verbose=True,
     memory=memory,
 )
 
-# print all
+# if you want only final output of agent stream
+# https://python.langchain.com/docs/modules/agents/how_to/streaming_stdout_final_only
 while True:
     print()
     user_input = prompt(console.print("Human: ", style="green"))
@@ -48,6 +56,3 @@ while True:
     console.print("AI: ", style="green")
     result = agent_chain.run(input=user_input)
     print(result)
-
-# if you want only final output of agent stream
-# https://python.langchain.com/docs/modules/agents/how_to/streaming_stdout_final_only
